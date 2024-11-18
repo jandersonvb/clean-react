1ec79affafb8b8fd65a247de1406fe4db6db7247
Janderson Vilas Boas <jandersonvb.dev@gmail.com>
2024-11-18 16:49:22 -0300

    test: ensure AxiosHttpClient calls axios with correct verb

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -3,20 +3,21 @@ import { AxiosHttpClient } from './axios-http-client'
 import axios from 'axios'
 
 jest.mock('axios')
-const mockedAxios = axios as jest.Mocked<typeof axios>
+const mockedAxios: jest.Mocked<Axios.AxiosStatic> = axios as jest.Mocked<typeof axios>
 
 const makeSut = (): AxiosHttpClient => {
   return new AxiosHttpClient()
 }
 
 describe('AxiosHttpClient', () => {
-  test('should call axios with correct URL', async () => {
+  test('should call axios with correct URL and verb', async () => {
     const url = faker.internet.url()
 
     const sut = makeSut()
 
     await sut.post({ url })
 
-    expect(mockedAxios).toHaveBeenCalledWith({ url })
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    expect(mockedAxios.post).toHaveBeenCalledWith(url)
   })
 })
